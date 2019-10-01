ce738266d38a46a91ac1ce9347b291af935f7bb7
Paulo Carvalho <pcarvalh@andrew.cmu.edu>
2019-10-01 18:30:33 -0400

    some house cleaning

--- loop_script.R
+++ loop_script.R
@@ -4,48 +4,147 @@ source(file = "models_function.R")
 dataset_list <- fread("datasets.csv")
 
 for(dataset in 1:nrow(dataset_list)){
-  m_itafm <- itAFM(dataset = dataset_list$StudentStep[dataset],kcm = dataset_list$KC[dataset],response = "First Attempt",opportunity = dataset_list$Opportunity[dataset],individual = "Anon Student Id")
-  
-  #output the KC model name
-  assign(paste("itafm_std.parms",dataset_list$Dataset[dataset],sep = "_"),m_itafm$stud.params)
-  
-  assign(paste("itafm_kc.parms",dataset_list$Dataset[dataset],sep = "_"),m_itafm$kc.params)
-  
-  assign(paste("itafm_parms",dataset_list$Dataset[dataset],sep = "_"),m_itafm$overall.params)
-  
-  assign(paste("itafm_steps_time",dataset_list$Dataset[dataset],sep = "_"),m_itafm$steps_time)
-  
-  assign(paste("itafm_df",dataset_list$Dataset[dataset],sep = "_"),m_itafm$df)
-  
-  print(paste("finished itAFM for",dataset_list$Dataset[dataset]))
-  
-  m_iafm <- iAFM(dataset = dataset_list$StudentStep[dataset],kcm = dataset_list$KC[dataset],response = "First Attempt",opportunity = dataset_list$Opportunity[dataset],individual = "Anon Student Id")
-  
-  #output the KC model name
-  assign(paste("iafm_std.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm$stud.params)
-  
-  assign(paste("iafm_kc.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm$kc.params)
-  
-  assign(paste("iafm_parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm$overall.params)
-  
-  assign(paste("iafm_df",dataset_list$Dataset[dataset],sep = "_"),m_iafm$df)
-  
-  print(paste("finished iAFM for",dataset_list$Dataset[dataset]))
-  
-  m_iafm_full <- iAFM_full(dataset = dataset_list$StudentStep[dataset],kcm = dataset_list$KC[dataset],response = "First Attempt",opportunity = dataset_list$Opportunity[dataset],individual = "Anon Student Id")
-  
-  #output the KC model name
-  assign(paste("iafm_full_std.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$stud.params)
-  
-  assign(paste("iafm_full_kc.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$kc.params)
-  
-  assign(paste("iafm_full_parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$overall.params)
-  
-  assign(paste("iafm_full_df",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$df)
-  
-  print(paste("finished iAFM_full for",dataset_list$Dataset[dataset]))
-  
+  tryCatch(
+    {
+      m_itafm <- itAFM(dataset = dataset_list$StudentStep[dataset],kcm = dataset_list$KC[dataset],response = "First Attempt",opportunity = dataset_list$Opportunity[dataset],individual = "Anon Student Id")
+      
+      #output the KC model name
+      assign(paste("itafm_std.parms",dataset_list$Dataset[dataset],sep = "_"),m_itafm$stud.params)
+      
+      assign(paste("itafm_kc.parms",dataset_list$Dataset[dataset],sep = "_"),m_itafm$kc.params)
+      
+      assign(paste("itafm_parms",dataset_list$Dataset[dataset],sep = "_"),m_itafm$overall.params)
+      
+      assign(paste("itafm_steps_time",dataset_list$Dataset[dataset],sep = "_"),m_itafm$steps_time)
+      
+      assign(paste("itafm_df",dataset_list$Dataset[dataset],sep = "_"),m_itafm$df)
+    },
+    error = function(e){
+      message("* Caught an error on dataset ", dataset_list$Dataset[dataset])
+      print(e)
+      },
+    finally = {
+      print(paste("finished itAFM for",dataset_list$Dataset[dataset]))
+    }
+  )
 }
 
+save(list=ls(),file = "modelFits_itAFM.RData")
 
-save(list=ls(),file = "modelFits.RData")
+#put together all student params for itAFM
+all_dataframes <- sapply(.GlobalEnv, is.data.frame)
+all_std.parms <- all_dataframes[grepl("std", names(all_dataframes))]
+
+itAFM_std.params <- do.call(rbind,mget(names(all_std.parms)[all_std.parms]))
+write.csv(itAFM_std.params,"itAFM_std.params.csv",row.names = F)
+
+all_kc.params <- all_dataframes[grepl("kc", names(all_dataframes))]
+itAFM_kc.params <- do.call(rbind,mget(names(all_kc.params)[all_kc.params]))
+write.csv(itAFM_kc.params,"itAFM_kc.params.csv",row.names = F)
+
+all.params <- all_dataframes[grepl("itafm_parms", names(all_dataframes))]
+itAFM.params <- do.call(rbind,mget(names(all.params)[all.params]))
+write.csv(itAFM.params,"itAFM.params.csv",row.names = F)
+
+steps.time <- all_dataframes[grepl("steps", names(all_dataframes))]
+itAFM.steps.time <- do.call(rbind,mget(names(steps.time)[steps.time]))
+write.csv(itAFM.steps.time,"itAFM.steps_time.csv",row.names = F)
+
+#clean environment
+rm(list=ls())
+source(file = "models_function.R")
+dataset_list <- fread("datasets.csv")
+
+for(dataset in 1:nrow(dataset_list)){
+  
+  tryCatch(
+    {
+      m_iafm <- iAFM(dataset = dataset_list$StudentStep[dataset],kcm = dataset_list$KC[dataset],response = "First Attempt",opportunity = dataset_list$Opportunity[dataset],individual = "Anon Student Id")
+      
+      #output the KC model name
+      assign(paste("iafm_std.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm$stud.params)
+      
+      assign(paste("iafm_kc.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm$kc.params)
+      
+      assign(paste("iafm_parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm$overall.params)
+      
+      assign(paste("iafm_df",dataset_list$Dataset[dataset],sep = "_"),m_iafm$df)
+      
+    },
+    error = function(e){
+      message("* Caught an error on dataset ", dataset_list$Dataset[dataset])
+      print(e)
+    },
+    finally = {
+      print(paste("finished iAFM for",dataset_list$Dataset[dataset]))
+    }
+  )
+}
+
+save(list=ls(),file = "modelFits_iAFM.RData")
+
+#put together all student params for iAFM
+all_dataframes <- sapply(.GlobalEnv, is.data.frame) 
+all_std.parms <- all_dataframes[grepl("std", names(all_dataframes))]
+
+iAFM_std.params <- do.call(rbind,mget(names(all_std.parms)[all_std.parms]))
+write.csv(iAFM_std.params,"iAFM_std.params.csv",row.names = F)
+
+all_kc.params <- all_dataframes[grepl("kc", names(all_dataframes))]
+iAFM_kc.params <- do.call(rbind,mget(names(all_kc.params)[all_kc.params]))
+write.csv(iAFM_kc.params,"iAFM_kc.params.csv",row.names = F)
+
+all.params <- all_dataframes[grepl("iafm_parms", names(all_dataframes))]
+iAFM.params <- do.call(rbind,mget(names(all.params)[all.params]))
+write.csv(iAFM.params,"iAFM.params.csv",row.names = F)
+
+#clean environment
+rm(list=ls())
+source(file = "models_function.R")
+dataset_list <- fread("datasets.csv")
+
+for(dataset in 1:nrow(dataset_list)){
+  
+  tryCatch(
+    {
+      m_iafm_full <- iAFM_full(dataset = dataset_list$StudentStep[dataset],kcm = dataset_list$KC[dataset],response = "First Attempt",opportunity = dataset_list$Opportunity[dataset],individual = "Anon Student Id")
+      
+      #output the KC model name
+      assign(paste("iafm_full_std.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$stud.params)
+      
+      assign(paste("iafm_full_kc.parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$kc.params)
+      
+      assign(paste("iafm_full_parms",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$overall.params)
+      
+      assign(paste("iafm_full_df",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$df)
+      
+      assign(paste("iafm_full_model",dataset_list$Dataset[dataset],sep = "_"),m_iafm_full$model)
+      
+    },
+    error = function(e){
+      message("* Caught an error on dataset ", dataset_list$Dataset[dataset])
+      print(e)
+    },
+    finally = {
+      print(paste("finished iAFM_full for",dataset_list$Dataset[dataset]))
+    }
+  )
+}
+
+save(list=ls(),file = "modelFits_iAFM_full.RData")
+
+#put together all student params for iAFM
+all_dataframes <- sapply(.GlobalEnv, is.data.frame) 
+all_std.parms <- all_dataframes[grepl("full_std", names(all_dataframes))]
+
+iAFM_full_std.params <- do.call(rbind,mget(names(all_std.parms)[all_std.parms]))
+write.csv(iAFM_full_std.params,"iAFM_full_std.params.csv",row.names = F)
+
+all_kc.params <- all_dataframes[grepl("full_kc", names(all_dataframes))]
+iAFM_full_kc.params <- do.call(rbind,mget(names(all_kc.params)[all_kc.params]))
+write.csv(iAFM_full_kc.params,"iAFM_full_kc.params.csv",row.names = F)
+
+all.params <- all_dataframes[grepl("iafm_full_parms", names(all_dataframes))]
+iAFM_full.params <- do.call(rbind,mget(names(all.params)[all.params]))
+write.csv(iAFM_full.params,"iAFM_full.params.csv",row.names = F)
+
--- models_function.R
+++ models_function.R
@@ -34,6 +34,12 @@ library(optimx)
 # opportunity = "Opportunity (LFASearchBICModel1-PVfixed-models uploaded)"
 # individual = "Anon Student Id"
 
+# dataset = dataset_list$StudentStep[5]
+# kcm = dataset_list$KC[5]
+# response = "First Attempt"
+# opportunity = dataset_list$Opportunity[5]
+# individual = "Anon Student Id"
+
 
 iAFM <- function(dataset, kcm,response,opportunity,individual){
   df = suppressWarnings(fread(file=paste(getwd(),"/data/",dataset,".txt",sep=""),verbose = F)) #the file to import
@@ -45,11 +51,19 @@ iAFM <- function(dataset, kcm,response,opportunity,individual){
   success <- ifelse(df$response=="correct",1,0) #recode response as 0 (incorrect) or 1 (correct)
   df$success <- success
   df$errorRate <- 1-success #add a success column
+  df$opportunity <- df$opportunity-1
+  
   rm(success)
   
-  iafm.model <- suppressWarnings(glmer(success ~ (1|individual) + (opportunity|individual) + (opportunity|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
+  #check number of opportunities per KC
+  #df <- as.data.table(df)
+  #checks <- df[,.(number_opp = length(individual),max = max(opportunity)),by=.(individual,KC)]
   
-  stud.params <- data.frame(cbind(row.names(ranef(iafm.model)$individual), ranef(iafm.model)$individual[,1], ranef(iafm.model)$individual[,2]) )
+  iafm.model <- suppressWarnings(glmer(success ~ (1|individual) + opportunity + (opportunity|individual) + (opportunity|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
+  
+  #iafm.model2 <- suppressWarnings(glmer(success ~ opportunity + (opportunity|individual) + (opportunity|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
+  
+  stud.params <- data.frame(cbind(row.names(ranef(iafm.model)$individual), ranef(iafm.model)$individual[,1], ranef(iafm.model)$individual[,3]) )
   stud.params <- cbind(Dataset = strsplit(dataset,split = "_")[[1]][1],Type="Student", stud.params)
   colnames(stud.params) <- c("Dataset","Type", "Student", "Intercept", "Slope")
   
@@ -60,10 +74,10 @@ iAFM <- function(dataset, kcm,response,opportunity,individual){
   model_AIC <- AIC(iafm.model)
   model_BIC <- BIC(iafm.model)
   model_logLik <- as.numeric(logLik(iafm.model))
-  maineffect_intercept <- fixef(iafm.model)[[1]]
-  #maineffect_slope <- fixef(iafm.model)[[2]]
+  #maineffect_intercept <- fixef(iafm.model)[[1]]
+  maineffect_slope <- fixef(iafm.model)[[1]]
   
-  overall.params <- data.frame(cbind(Dataset=strsplit(dataset,split = "_")[[1]][1],model_AIC,model_BIC,model_logLik,maineffect_intercept))
+  overall.params <- data.frame(cbind(Dataset=strsplit(dataset,split = "_")[[1]][1],model_AIC,model_BIC,model_logLik,maineffect_slope))
   
   df$predicted_error_rate <- 1-predict(iafm.model,df,type="response",allow.new.levels=TRUE)
   
@@ -97,14 +111,35 @@ itAFM <- function(dataset, kcm,response,opportunity,individual){
   library(plyr)
   df <- ddply(.data = df,.variables = .(individual,KC),.fun = mutate,first_oppTime =  time[rec_opportunity==1])
   
+  #df <- ddply(.data = df,.variables = .(individual,KC),.fun = mutate,first_oppTime =  time[opportunity==1])
+  
   #calculate difference in time from first step
-  df$timeDiff <- as.numeric(difftime(df$time,df$first_oppTime,units="mins"))
+  df$timeDiff <- as.numeric(as.character(difftime(df$time,df$first_oppTime,units="secs")))
+  df$timeDiff <- ifelse(is.na(df$timeDiff)&!is.na(df$rec_opportunity),0,df$timeDiff)
+  #df$scaled_diff <- scale(df$timeDiff)
   
- # df$timeDiff <- ifelse(df$rec_opportunity)
+  #transform time series because of heteroskedasticity  using Box-Cox transformation.
+  #library(forecast)
+  #lambda.value <- BoxCox.lambda(df$timeDiff)
+  #df$scaled_diff <- BoxCox(df$timeDiff,lambda=0.5)
   
-  itafm.model <- suppressWarnings(glmer(success ~ (1|individual) + (timeDiff|individual) + (timeDiff|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
+  #df <- ddply(.data = df,.variables = .(individual,KC),.fun = mutate,scaled_diff = BoxCox(timeDiff,lambda=0.5))
+  #df$scaled_diff <- ifelse(is.nan(df$scaled_diff),0,df$scaled_diff)
   
-  stud.params <- data.frame(cbind(row.names(ranef(itafm.model)$individual), ranef(itafm.model)$individual[,1], ranef(itafm.model)$individual[,2]) )
+  #check number of opportunities per KC
+  #df <- as.data.table(df)
+  #checks4 <- df[,.(number_opp = length(individual),max = max(opportunity)),by=.(individual,KC)]
+  
+  #df <- df[!df$KC=="",]
+  #df$timeDiff <- ifelse(df$rec_opportunity)
+  
+  #itafm.model <- suppressWarnings(glmer(success ~ (1|individual) + scaled_diff + (scaled_diff|individual) + (scaled_diff|KC) - 1, data=df[!is.na(df$rec_opportunity),], family=binomial(),control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+  
+  itafm.model <- suppressWarnings(glmer(success ~ (1|individual) + timeDiff + (timeDiff|individual) + (timeDiff|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
+  
+  #itafm.model <- suppressWarnings(glmer(success ~ (1|individual) + (timeDiff|individual) + (timeDiff|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
+  
+  stud.params <- data.frame(cbind(row.names(ranef(itafm.model)$individual), ranef(itafm.model)$individual[,1], ranef(itafm.model)$individual[,3]) )
   stud.params <- cbind(Dataset = strsplit(dataset,split = "_")[[1]][1],Type="Student", stud.params)
   colnames(stud.params) <- c("Dataset","Type", "Student", "Intercept", "Slope")
   
@@ -115,10 +150,10 @@ itAFM <- function(dataset, kcm,response,opportunity,individual){
   model_AIC <- AIC(itafm.model)
   model_BIC <- BIC(itafm.model)
   model_logLik <- as.numeric(logLik(itafm.model))
-  maineffect_intercept <- fixef(itafm.model)[[1]]
-  #maineffect_slope <- fixef(itafm.model)[[2]]
+  #maineffect_intercept <- fixef(itafm.model)[[1]]
+  maineffect_slope <- fixef(itafm.model)[[1]]
   
-  overall.params <- data.frame(cbind(Dataset=strsplit(dataset,split = "_")[[1]][1],model_AIC,model_BIC,model_logLik,maineffect_intercept))
+  overall.params <- data.frame(cbind(Dataset=strsplit(dataset,split = "_")[[1]][1],model_AIC,model_BIC,model_logLik,maineffect_slope))
   
   df$predicted_error_rate <- 1-predict(itafm.model,df,type="response",allow.new.levels=TRUE)
   
@@ -126,6 +161,7 @@ itAFM <- function(dataset, kcm,response,opportunity,individual){
   
   df <- as.data.table(df)
   steps_time <- df[!time_opp==0,.(n_opp=max(opportunity),max_time_btw_opp=max(time_opp)),by=.(individual,KC)]
+  steps_time$Dataset <- strsplit(dataset,split = "_")[[1]][1]
   
   return(list(stud.params=stud.params,kc.params=kc.params,overall.params=overall.params,df=df,steps_time=steps_time))
 }
@@ -157,30 +193,41 @@ iAFM_full <- function(dataset, kcm,response,opportunity,individual){
   library(plyr)
   df <- ddply(.data = df,.variables = .(individual,KC),.fun = mutate,first_oppTime =  time[rec_opportunity==1])
   
+  #df <- ddply(.data = df,.variables = .(individual,KC),.fun = mutate,first_oppTime =  time[opportunity==1])
+  
   #calculate difference in time from first step
-  df$timeDiff <- as.numeric(difftime(df$time,df$first_oppTime,units="mins"))
+  df$timeDiff <- as.numeric(as.character(difftime(df$time,df$first_oppTime,units="secs")))
+  df$timeDiff <- ifelse(is.na(df$timeDiff)&!is.na(df$rec_opportunity),0,df$timeDiff)
+  #df$scaled_diff <- scale(df$timeDiff)
   
-  iafm_full.model <- suppressWarnings(glmer(success ~ (1|individual) + (timeDiff|individual) + (timeDiff|KC) + (opportunity|individual) + (opportunity|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+  #transform time series because of heteroskedasticity  using Box-Cox transformation.
+  #library(forecast)
+  #lambda.value <- BoxCox.lambda(df$timeDiff)
+  #df$scaled_diff <- BoxCox(df$timeDiff,lambda=0.5)
   
-  stud.params <- data.frame(cbind(row.names(ranef(iafm_full.model)$individual), ranef(iafm_full.model)$individual[,3], ranef(iafm_full.model)$individual[,4],ranef(iafm_full.model)$individual[,1],ranef(iafm_full.model)$individual[,2]))
+ # iafm_full.model <- suppressWarnings(glmer(success ~ (1|individual) + opportunity + scaled_diff + ((scaled_diff+opportunity)|individual) + ((scaled_diff+opportunity)|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+  
+  iafm_full.model <- suppressWarnings(glmer(success ~ (1|individual) + opportunity + timeDiff + ((timeDiff+opportunity)|individual) + ((timeDiff+opportunity)|KC) - 1, data=df, family=binomial(),control = glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+  
+  stud.params <- data.frame(cbind(row.names(ranef(iafm_full.model)$individual), ranef(iafm_full.model)$individual[,1], ranef(iafm_full.model)$individual[,2],ranef(iafm_full.model)$individual[,3]))
   stud.params <- cbind(Dataset = strsplit(dataset,split = "_")[[1]][1],Type="Student", stud.params)
-  colnames(stud.params) <- c("Dataset","Type", "Student","Intercept_time","Slope_time","Intercept_opp", "Slope_opp")
+  colnames(stud.params) <- c("Dataset","Type", "Student","Intercept","Slope_time", "Slope_opp")
   
-  kc.params <- data.frame(cbind(row.names(ranef(iafm_full.model)$KC), ranef(iafm_full.model)$KC[,3], ranef(iafm_full.model)$KC[,4]),ranef(iafm_full.model)$KC[,1],ranef(iafm_full.model)$KC[,2])
+  kc.params <- data.frame(cbind(row.names(ranef(iafm_full.model)$KC), ranef(iafm_full.model)$KC[,1], ranef(iafm_full.model)$KC[,2]),ranef(iafm_full.model)$KC[,3])
   kc.params <- cbind(Dataset = strsplit(dataset,split = "_")[[1]][1],Type="KC", kc.params)
-  colnames(kc.params) <- c("Dataset","Type", "KC","Intercept_time","Slope_time", "Intercept_opp", "Slope_opp")
+  colnames(kc.params) <- c("Dataset","Type", "KC","Intercept","Slope_time","Slope_opp")
   
   model_AIC <- AIC(iafm_full.model)
   model_BIC <- BIC(iafm_full.model)
   model_logLik <- as.numeric(logLik(iafm_full.model))
-  maineffect_intercept <- fixef(iafm_full.model)[[1]]
-  #maineffect_slope <- fixef(iafm_full.model)[[2]]
+  #maineffect_intercept <- fixef(iafm_full.model)[[1]]
+  maineffect_slope <- fixef(iafm_full.model)[[1]]
   
-  overall.params <- data.frame(cbind(Dataset=strsplit(dataset,split = "_")[[1]][1],model_AIC,model_BIC,model_logLik,maineffect_intercept))
+  overall.params <- data.frame(cbind(Dataset=strsplit(dataset,split = "_")[[1]][1],model_AIC,model_BIC,model_logLik,maineffect_slope))
   
   df$predicted_error_rate <- 1-predict(iafm_full.model,df,type="response",allow.new.levels=TRUE)
   
-  return(list(stud.params=stud.params,kc.params=kc.params,overall.params=overall.params,df=df))
+  return(list(stud.params=stud.params,kc.params=kc.params,overall.params=overall.params,df=df,model=iafm_full.model))
 }
 
 
